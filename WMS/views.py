328a8162cf674416c04c6a797e2da2bbe40a1307
munjihwan020627 <85243418+munjihwan020627@users.noreply.github.com>
2025-06-29 13:18:50 +0900

    Update views.py
    로그인 구현

--- WMS/views.py
+++ WMS/views.py
@@ -6,28 +6,32 @@ from .models import Product
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='common:login')
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
     return render(request, 'WMS/product_list.html', {'products': products})
 
 
+@login_required(login_url='common:login')
 def add_product(request):
     if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)  # ✅ request.FILES 추가
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save(commit=False)
+            product.user = request.user  # 작성자 설정
+            product.save()
             messages.success(request, '상품이 성공적으로 추가되었습니다.')
             return redirect('product_list')
     else:
         form = ProductForm()
     return render(request, 'WMS/add_product.html', {'form': form})
 
-
+@login_required(login_url='common:login')
 def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk, user=request.user)  # 본인 것만
     if request.method == 'POST':
-        product = get_object_or_404(Product, pk=pk)
         product.delete()
         messages.success(request, '상품이 삭제되었습니다.')
     return redirect('product_list')
