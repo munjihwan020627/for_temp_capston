9c3ef666f5cd90e98a08833d4e1f53f860659f1d
munjihwan020627 <85243418+munjihwan020627@users.noreply.github.com>
2025-06-29 13:19:10 +0900

    Update models.py

--- WMS/models.py
+++ WMS/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -6,6 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='products/', blank=True, null=True)  # ✅ 없으면 추가
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
